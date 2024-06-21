1536739fa5948e7bfc737d46c0c0b0c90a64301f
Kyumin3 <101244527+Kyumin3@users.noreply.github.com>
2024-06-21 15:00:08 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import { useState } from 'react'
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false)
+  return (
+    <div className="App">
+      <header className='app-header'>
+        <h3 data-testId="counter">{count}</h3>
+        <button data-testId='minus-button' onClick={() => { if(count>0)setCount((prev) => prev -1)}} disabled={disabled}>-</button>
+        <button data-testId='plus-button' onClick={() => setCount((prev) => prev +1)} disabled={disabled}>+</button>
+        <div>
+          <button style={{backgroundColor : "blue"}} data-testId="on/off-button" onClick={()=> setDisabled((prev) => !prev)}>
+            on/off
+          </button>
+
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
